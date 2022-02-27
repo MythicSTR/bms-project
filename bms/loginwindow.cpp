f838da6db318b76288fb95727405a0207d4b292c
Shishir Tamrakar <stamrakar545@gmail.com>
2022-02-27 17:27:53 +0545

    Added Qt files

---
+++ bms/loginwindow.cpp
@@ -0,0 +1,15 @@
+#include "loginwindow.h"
+#include "ui_loginwindow.h"
+
+LogInWindow::LogInWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::LogInWindow)
+{
+    ui->setupUi(this);
+}
+
+LogInWindow::~LogInWindow()
+{
+    delete ui;
+}
+
---
+++ bms/loginwindow.h
@@ -0,0 +1,21 @@
+#ifndef LOGINWINDOW_H
+#define LOGINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class LogInWindow; }
+QT_END_NAMESPACE
+
+class LogInWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    LogInWindow(QWidget *parent = nullptr);
+    ~LogInWindow();
+
+private:
+    Ui::LogInWindow *ui;
+};
+#endif // LOGINWINDOW_H
